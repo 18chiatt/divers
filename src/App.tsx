f254fe7ab7c304ac9839da3e0dd5a0acae31c993
Chase Hiatt <18chiattinamerica@gmail.com>
2024-05-02 21:13:52 -0700

    add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
-import { Box, Button, Select } from "@mui/material";
+import { Box, Button, Select, Tooltip, Typography } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import useSound from "use-sound";
 import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
 import ForwardIcon from "@mui/icons-material/Forward";
-import { VolumeOff } from "@mui/icons-material";
+import { AccessTimeOutlined, EmojiEvents, TimelineOutlined, VolumeOff } from "@mui/icons-material";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 
 const EASY: Policy = {
@@ -59,6 +59,72 @@ enum Direction {
 	right = "right",
 }
 
+function useHighscoreState(difficulty: string) {
+	const [highscore, setHighscore] = useState<number | null>(null);
+	const key = `${difficulty}-${MAX_TIMES_TO_CONSIDER}`;
+
+	const updateHighscore = useCallback(
+		(newV: number | null) => {
+			setHighscore(newV);
+			if (newV) {
+				localStorage.setItem(key, newV.toString());
+			}
+		},
+		[key, setHighscore]
+	);
+
+	useEffect(() => {
+		const existingHighscore = parseFloat(localStorage.getItem(key) ?? "99999999999");
+		updateHighscore(existingHighscore < 1000 ? existingHighscore : null);
+	}, [difficulty, key, updateHighscore]);
+
+	const proposeHighscore = useCallback(
+		(times: number[]) => {
+			if (times.length !== MAX_TIMES_TO_CONSIDER) {
+				return;
+			}
+			const currTime = times.reduce((p, c) => p + c, 0) / MAX_TIMES_TO_CONSIDER / 1000;
+			if (!highscore || currTime < highscore) {
+				updateHighscore(currTime);
+			}
+		},
+		[updateHighscore, highscore]
+	);
+	return { highscore, proposeHighscore };
+}
+
+const MAX_TIMES_TO_CONSIDER = 2;
+
+function useTimerState(difficulty: string) {
+	const [previousTime, setPreviousTime] = useState<number>(0);
+	const [startTime, setStartTime] = useState(new Date());
+	const [times, setTimes] = useState<number[]>([]);
+	const { highscore, proposeHighscore } = useHighscoreState(difficulty);
+	const finish = useCallback(
+		(success: boolean) => {
+			const now = new Date();
+			const time = now.getTime() - startTime.getTime();
+			if (success) {
+				setTimes(times.length === MAX_TIMES_TO_CONSIDER ? [...times.slice(1), time] : [...times, time]);
+				setPreviousTime(time);
+				proposeHighscore(times);
+			}
+		},
+		[startTime, setPreviousTime, times, proposeHighscore]
+	);
+
+	const beginTimer = useCallback(() => {
+		setStartTime(new Date());
+	}, [setStartTime]);
+
+	const clearStats = useCallback(() => {
+		setPreviousTime(0);
+		setStartTime(new Date());
+		setTimes([]);
+	}, [setPreviousTime, setStartTime, setTimes]);
+	return { previousTime, times, finish, clearStats, beginTimer, highscore };
+}
+
 const darkTheme = createTheme({
 	palette: {
 		mode: "dark",
@@ -71,11 +137,17 @@ function App() {
 	const [policy, setPolicy] = useState<Difficulty>(EASY);
 	const [stateIndication, setStateIndication] = useState<StateIndication>(StateIndication.Normal);
 	const [mute, setMute] = useState(false);
+	const { previousTime, finish, times, clearStats, beginTimer, highscore } = useTimerState(policy.label);
 	const policyRef = useRef(policy);
 	const [click] = useSound("/divers/click.mp3", { volume: mute ? 0.00000001 : 1 });
 	const [failureSound] = useSound("/divers/failure.mp3", { volume: mute ? 0.0000001 : 1 });
 	const [successSound] = useSound("/divers/success.mp3", { volume: mute ? 0.000001 : 1 });
 
+	useEffect(() => {
+		if (progress === 1) {
+			beginTimer();
+		}
+	}, [progress, beginTimer]);
 	useEffect(() => {
 		policyRef.current = policy;
 	}, [policyRef, policy]);
@@ -85,23 +157,28 @@ function App() {
 		setStateIndication(StateIndication.Normal);
 	}, [setCombo, setProgress, policyRef]);
 
-	useEffect(() => reset(), [reset, policy]);
+	useEffect(() => {
+		reset();
+		clearStats();
+	}, [reset, policy, clearStats]);
 
 	const initiateError = useCallback(() => {
 		setStateIndication(StateIndication.Error);
 		failureSound();
+		finish(false);
 		setTimeout(() => {
 			reset();
 		}, ERROR_FLASHING_TIME);
-	}, [setStateIndication, reset, failureSound]);
+	}, [setStateIndication, reset, failureSound, finish]);
 
 	const success = useCallback(() => {
 		setStateIndication(StateIndication.Success);
 		successSound();
+		finish(true);
 		setTimeout(() => {
 			reset();
 		}, ERROR_FLASHING_TIME);
-	}, [setStateIndication, reset, successSound]);
+	}, [setStateIndication, reset, successSound, finish]);
 
 	const onKeyDown = useCallback(
 		(key: Direction) => {
@@ -141,8 +218,10 @@ function App() {
 		<ThemeProvider theme={darkTheme}>
 			<AppContainer>
 				<TerminalScreen currCombo={combo} progress={progress} gameState={stateIndication} />
-
-				<DifficultyControls policy={policy} setPolicy={setPolicy} mute={mute} setMute={setMute} />
+				<Box marginBottom="100px">
+					<Scores previousTime={previousTime} times={times} highscore={highscore} />
+					<DifficultyControls policy={policy} setPolicy={setPolicy} mute={mute} setMute={setMute} />
+				</Box>
 			</AppContainer>
 		</ThemeProvider>
 	);
@@ -263,6 +342,60 @@ function calculateClassName(version: ArrowState) {
 	return "baseArrow normalArrow";
 }
 
+const PLACEHOLDER_WIDTH = "80px";
+
+function Scores(props: { previousTime: number; times: number[]; highscore: number | null }) {
+	const { previousTime, times, highscore } = props;
+	const timeSum = times.reduce((prev, curr) => prev + curr, 0);
+
+	return (
+		<Box display="flex" justifyContent="center" marginBottom="30px">
+			<Tooltip title="Previous Time.  Timer starts after first keypress">
+				<Typography
+					sx={{
+						color: "white",
+						display: "flex",
+						alignItems: "center",
+						marginRight: "15px",
+						minWidth: PLACEHOLDER_WIDTH,
+					}}
+				>
+					<AccessTimeOutlined sx={{ marginRight: "4px" }} />
+					{previousTime ? `${(previousTime / 1000).toFixed(3)}s` : ""}
+				</Typography>
+			</Tooltip>
+			<Tooltip title={"Average time over last " + times.length + " succesful games"}>
+				<Typography
+					sx={{
+						color: "white",
+						display: "flex",
+						alignItems: "center",
+						marginRight: "15px",
+						minWidth: PLACEHOLDER_WIDTH,
+					}}
+				>
+					<TimelineOutlined sx={{ marginRight: "4px" }} />{" "}
+					{times.length ? `${(timeSum / 1000 / times.length).toFixed(3)}s` : ""}
+				</Typography>
+			</Tooltip>
+
+			<Tooltip title={"Best average over " + MAX_TIMES_TO_CONSIDER}>
+				<Typography
+					sx={{
+						color: "white",
+						display: "flex",
+						alignItems: "center",
+						marginRight: "15px",
+						minWidth: PLACEHOLDER_WIDTH,
+					}}
+				>
+					<EmojiEvents sx={{ marginRight: "4px" }} /> {highscore ? `${highscore.toFixed(3)}s` : ""}
+				</Typography>
+			</Tooltip>
+		</Box>
+	);
+}
+
 function DifficultyControls(props: {
 	policy: Policy;
 	setPolicy: (newPolicy: Policy) => void;
@@ -270,8 +403,9 @@ function DifficultyControls(props: {
 	setMute: (newV: boolean) => void;
 }) {
 	const { policy, setPolicy, mute, setMute } = props;
+
 	return (
-		<Box display="flex" justifyContent="center" marginBottom="100px">
+		<Box display="flex" justifyContent="center" alignItems="center">
 			<Select
 				onChange={(v): void => {
 					const difficultyStr = v.target.value;
